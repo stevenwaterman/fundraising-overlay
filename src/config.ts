6d9c12971efa89c2a66ee6c8b41179bdaecf5541
steven <github@stevenwaterman.uk>
2021-06-22 16:58:32 +0100

    Use donation page that updates more often instead

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const url = "https://www.blacktransalliance.org/fundraisers/goldie-gang-supporting-black-trans-alliance";
-export const targetSelector = "#targetAmount > .totaliserFigure > span";
-export const currentSelector = "#raisedAmount > .totaliserFigure > span";
-export const decimals = 2;
+export const url = "https://www.blacktransalliance.org/donate/goldie-gang-supporting-black-trans-alliance/0/credit-card";
+export const targetSelector = ".targetAmount > span";
+export const currentSelector = ".raisedAmount > span";
+export const decimals = 0;
 export const symbol = "£";
